8c128463afdd6b611445706b7e222dc207381f52
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-11-20 01:10:52 +0900

    Delete frontend/src/api directory

--- frontend/src/api/http.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const http = axios.create({
-  // localhost 대신 실제 IP 주소 사용
-  baseURL: "http://localhost:8097/fitquest/api",
-  withCredentials: true,
-});
-
-export default http;
