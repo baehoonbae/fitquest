cc95d2656411f6a4cdddfb4a78797d8f65c9e9ec
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-11-30 00:07:16 +0900

    refactor: 불필요 클래스 제거

--- backend/src/main/java/com/web/fitquest/mapper/article/ArticleMapper.java
+++
@@ -1,8 +0,0 @@
-package com.web.fitquest.mapper.article;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ArticleMapper {
-    
-}
--- backend/src/main/java/com/web/fitquest/service/article/ArticleServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.web.fitquest.service.article;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ArticleServiceImpl implements ArticleService {
-    
-}
