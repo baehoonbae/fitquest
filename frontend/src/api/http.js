c2f150852cf331c9703babd33c60d3211e2cd32d
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-12-04 00:08:58 +0900

    chore: Base URL 수정

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8097/fitquest/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8097/fitquest/api";
 
 const http = axios.create({
   baseURL: baseURL,
