de381f90510b81e3f843f3236e8e43ea0a498e93
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-12-04 03:26:00 +0900

    refactor: CORS 관련 설정

--- backend/src/main/java/com/web/fitquest/config/WebConfig.java
+++ backend/src/main/java/com/web/fitquest/config/WebConfig.java
@@ -22,31 +22,20 @@ public class WebConfig implements WebMvcConfigurer {
         registry
             .addMapping("/**")
             .allowedOrigins(
-                "http://localhost:5173",     // Vue 개발 서버
-                "http://localhost:8097",     // Spring 서버
-                "http://70.12.50.63:8097",    // IP 주소로 접근
-                "http://70.12.50.62:8097",    // IP 주소로 접근
-                "https://3.24.232.172",    // IP 주소로 접근
+                "http://localhost:5173",
                 "https://elaborate-jelly-ef12f0.netlify.app"
-                // "http://192.168.10.10:8097",    //  성훈 집 ip로 바꾸기
-                // "http://70.12.50.63:8097"    // 채준 집 ip로 바꾸기
             )
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600);
 
-        // fitquest 전용 엔드포인트에 대한 설정도 동일하게 수정
+        // fitquest 전용 엔드포인트에 대한 설정
         registry
             .addMapping("/fitquest/api/**")
             .allowedOrigins(
                 "http://localhost:5173",
-                "http://localhost:8097",
-                "http://70.12.50.62:8097",
-                "https://3.24.232.172",
                 "https://elaborate-jelly-ef12f0.netlify.app"
-                // "http://192.168.10.10:8097",    //  성훈 집 ip로 바꾸기
-                // "http://70.12.50.63:8097"    // 채준 집 ip로 바꾸기
             )
             .allowedMethods("*")
             .allowedHeaders("*")
